a3131d3f107c51bc3498548c14381912086a7c9a
Jael <jhael.nicolalde@epn.edu.ec.com>
2023-07-26 10:31:55 -0500

    uploaded project

---
+++ database/migrations/2023_07_16_183810_update_personal_access_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePersonalAccessTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            // Agregar/modificar columnas según tus necesidades
+            $table->timestamp('expires_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            // Revertir los cambios si es necesario
+            $table->dropColumn('expires_at');
+        });
+    }
+}
+
